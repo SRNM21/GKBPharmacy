9627feb0d30e7b34abe1eee54912f270840daa6b
SRNM21 <gregorio1nonly@gmail.com>
2023-12-26 20:06:17 +0800

    deb

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GKBPharmacy.MainWindow
+            Me.MainForm = Global.GKBPharmacy.LogInForm
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
